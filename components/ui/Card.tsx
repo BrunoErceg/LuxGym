eff89b3b55cb0e6b62e3154b83fdcf405115d25f
Bruno Erceg <brunoerceg95@gmail.com>
2026-01-11 15:34:03 +0100

    refactor: add props support, className, and HTMLDivElement types to Card

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,32 +1,37 @@
 import Image from 'next/image';
-import Animated from '@/components/animations/InViewAnimation';
 import Heading from '@ui/Heading';
 import Typography from './Typography';
+import { HTMLAttributes } from 'react';
+import { cn } from '@/utils/cn';
 
-type CardProps = {
+type CardProps = HTMLAttributes<HTMLDivElement> & {
   title?: string;
   description?: string;
   icon: string;
-  animationDelay?: number;
 };
 
-function Card({ title, description, icon, animationDelay = 0 }: CardProps) {
+/**
+ * A React component that displays a card with a title, description and icon.
+ * The component is used to display information about a skill or project.
+ * The component is a wrapper around the Heading and Typography components.
+ *
+ * @returns - The JSX element of the component.
+ */
+function Card({ title, description, icon, className, ...props }: CardProps) {
   return (
-    <Animated delay={animationDelay}>
-      <div className="space-y-4">
-        <Image
-          src={icon}
-          width={94}
-          height={94}
-          alt=""
-          className="mx-auto size-18 md:mx-0 md:size-24"
-        />
-        <Heading level={3}>{title}</Heading>
-        <Typography variant="body" className="mx-[10%] text-center md:mx-0 md:text-left">
-          {description}
-        </Typography>
-      </div>
-    </Animated>
+    <div className={cn('space-y-4', className)} {...props}>
+      <Image
+        src={icon}
+        width={94}
+        height={94}
+        alt=""
+        className="mx-auto size-18 md:mx-0 md:size-24"
+      />
+      <Heading level={3}>{title}</Heading>
+      <Typography variant="body" className="mx-[10%] text-center md:mx-0 md:text-left">
+        {description}
+      </Typography>
+    </div>
   );
 }
 
